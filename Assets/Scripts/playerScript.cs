7ac4a95d4932693c90c47a1c692b6f9bb0f14a15
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-11-09 16:03:33 +0100

    DOne?

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+
+    public float moveSpeed = 6f;
+    public float jumpSpeed = 12f;
+
+    private Rigidbody2D rbody;
+    public groundCheck groundcheck;
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") *moveSpeed, rbody.velocity.y);
+        //Input.GetAxis("Horizontal");
+        Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(groundcheck.isGrounded == true)
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpSpeed);
+        }
+
+    }
+}
