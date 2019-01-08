d37d5bb82b8ccd25c629dbed096b52bdc87b361e
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2019-01-08 10:28:54 +0100

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ToggleButton(GameObject obj)
+    {
+        obj.GetComponent<Button>().interactable
+            = !obj.GetComponent<Button>().interactable;
+    }
+}
---
+++ Assets/Scripts/TriangleRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleRotate : MonoBehaviour
+{
+    public void Rotate(float amount)
+    {
+        transform.Rotate(0, 0, amount);
+    }
+
+
+}
